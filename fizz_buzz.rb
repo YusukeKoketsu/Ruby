3b09c94481103132d58c39d9e1bd5d37001f6212
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-07-29 14:58:24 +0000

    ruby

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,19 +1,45 @@
 # 自分で好きな数字を入力する
-def fizz_buzz(number)
-  if number % 15 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
+# def fizz_buzz(number)
+#   if number % 15 == 0
+#     "FizzBuzz"
+#   elsif number % 3 == 0
+#     "Fizz"
+#   elsif number % 5 == 0
+#     "Buzz"
+#   else
+#     number.to_s
+#   end
+# end
+
+# puts "数字を入力してください。"
+
+# input = gets.to_i
+
+# puts '結果は...'
+# puts fizz_buzz(input)
+
+# eachを使用し範囲指定1~100
+# (1..100).each do |num|
+#   if num % 15 == 0
+#     puts "FizzBuzz"
+#   elsif num % 3 == 0
+#     puts "Fizz"
+#   elsif num % 5 == 0
+#     puts "Buzz"
+#   else
+#     puts num.to_i
+#   end
+# end
+
+# forを使用し範囲指定1~30
+for num in 1..30
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
   else
-    number.to_s
+    puts num.to_i
   end
 end
-
-puts "数字を入力してください。"
-
-input = gets.to_i
-
-puts '結果は...'
-puts fizz_buzz(input)
--- progate.rb
+++ progate.rb
@@ -1,5 +1,8 @@
 languages = ["日本語", "英語", "スペイン語"]
 
+# 要素の中身をすべて表示
+puts languages
+
 # インデックス番号が1の要素を出力してください
 puts languages[1]
 
@@ -10,3 +13,82 @@ puts "#{languages[0]}を話せます"
 languages.each do |language|
   puts "#{language}を話せます"
 end
+
+exam = {"subject" => "Math", "score" => 80}
+
+# キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キー「"subject"」の値を「"Science"」に更新してください
+# 書き換えが可能
+exam["subject"] = "Science"
+
+# もう一度、キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キーが「"grade"」、値が「"good"」の要素を追加してください
+exam["grade"] = "good"
+
+# キー「"grade"」の値を出力してください
+puts exam["grade"]
+
+# 省略した書き方で書き換えてください
+exam = {subject: "Math", score: 80}
+
+puts "#{exam[:subject]}: #{exam[:score]}点"
+
+# examにscoreがなくnilになるため、47行目が実行される
+exam = {subject: "Math"}
+
+# 条件式にキー「:score」の値を使うif文をつくってください
+if exam[:score]
+  puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+else
+  puts "#{exam[:subject]}の結果は分かりません"
+end
+
+
+# 配列[インデックス番号][キー]で、特定のハッシュの値を用いることができる インデックス番号は上から0,1,2...となる
+exams = [
+  {subject: "Math", score: 80},
+  {subject: "Science", score: 55}
+]
+
+# インデックス番号が1の要素の、キーが「:score」の値を出力してください
+# 55が表示
+puts exams[1][:score]
+
+# each文を用いて、要素ごとに「○○の結果は△△点です」と出力してください
+exams.each do |exam|
+ puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+end
+
+characters = [
+  {name: "にんじゃわんこ", age: 14},
+  {name: "ひつじ仙人"},
+  {name: "ベイビーわんこ", age: 5},
+  {name: "とりずきん"}
+]
+
+characters.each do |character|
+  puts "--------------------"
+  puts "名前は#{character[:name]}です"
+
+  # キー:ageの値に応じて年齢の情報を出力してください
+  # ageがあれば表示
+  if character[:age]
+    puts "年齢は#{character[:age]}歳です"
+  else
+    puts "年齢は秘密です"
+  end
+end
+
+# 「def メソッド名(引数名)」とすることで、引数を指定することできる
+# 引数を渡してメソッドを呼び出すには、「メソッド名(値)」とする  引数（ひきすう）とはメソッドに与える追加情報
+def introduce(name)
+  puts "こんにちは"
+  puts "私は#{name}です"
+end
+
+# 引数を渡してメソッドを呼び出してください
+introduce("koketsu")
