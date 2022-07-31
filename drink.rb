4187d2a3352c9acf5740b08cba1e15f4a448d447
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-07-31 14:59:52 +0000

    ruby

--- drink.rb
+++ drink.rb
@@ -3,4 +3,7 @@ require "./menu"
 # DrinkはMenuの子クラスになるため、name,price,amountの3つのインスタンス変数を持つことになる
 class Drink < Menu
   attr_accessor :amount
+  def info
+    return "#{self.name} #{self.price}円 (#{self.amount}mL)"
+  end
 end
